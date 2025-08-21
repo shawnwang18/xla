29ab29cc129c09788121ddb7c109da76a20100aa
Shawn Wang <shawnw@nvidia.com>
2025-08-21 00:54:41 -0700

    fix

--- xla/backends/gpu/runtime/command_buffer_cmd.cc
+++ xla/backends/gpu/runtime/command_buffer_cmd.cc
@@ -365,7 +365,7 @@ absl::StatusOr<CommandBufferCmdExecutor> CommandBufferCmdExecutor::Create(
   // In automatic synchronization mode construct an execution graph for the
   // sequence of commands and derive the structure of command dependencies
   // from the buffer use conflicts.
-  if (synchronization_mode != SynchronizationMode::kSequential) {
+  if (synchronization_mode != SynchronizationMode::kSerialize) {
     auto operations = CreateCommandOperations(commands, synchronization_mode);
     TF_ASSIGN_OR_RETURN(execution_graph,
                         ExecutionGraph::Create<CommandOperation>(operations));
@@ -695,7 +695,7 @@ absl::StatusOr<std::string> CommandBufferCmdExecutor::RenderExecutionGraph() {
     return Unimplemented("No execution graph renderer registered");
   }
 
-  if (synchronization_mode_ == SynchronizationMode::kSequential) {
+  if (synchronization_mode_ == SynchronizationMode::kSerialize) {
     return Unimplemented(
         "Execution graph rendering is only supported for "
         "concurrent/LHS synchronization mode");
